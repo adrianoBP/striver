c5c01d6ce04832ed987a42347871eaf889fb17f0
adrianoBP <18449283+adrianoBP@users.noreply.github.com>
2022-03-19 12:47:19 +0000

    Add connection to MongoDB

---
+++ controllers/testing.controller.js
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import * as dbService from '../services/db.service.js';
+import { v4 as uuidv4 } from 'uuid';
+
+const router = new Router();
+
+const test = (req, res) => {
+  res.send('Hello world!');
+};
+
+const insertRecord = async (req, res) => {
+  const newId = uuidv4();
+
+  const dbClient = dbService.getConnection();
+  await dbClient.connect();
+  dbService.insertRecord(dbClient, 'test', { name: 'test', _id: newId });
+
+  res.send(`Inserted record with id ${newId}`);
+};
+
+const getRecords = async (req, res) => {
+  const { recordId } = req.body;
+
+  const dbClient = dbService.getConnection();
+  await dbClient.connect();
+  const records = await dbService.getRecord(dbClient, 'test', recordId);
+
+  res.send(records);
+};
+
+router.get('/', test);
+router.post('/insert', insertRecord);
+router.post('/get', getRecords);
+
+export default router;
