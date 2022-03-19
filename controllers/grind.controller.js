6c098055652108e53e0b4ef3c9f8053e237d0209
adrianoBP <18449283+adrianoBP@users.noreply.github.com>
2022-03-19 19:31:27 +0000

    Complete grind service

--- controllers/grind.controller.js
+++ controllers/grind.controller.js
@@ -8,6 +8,8 @@ const router = new Router();
 const getGrind = async (req, res) => {
   const { grindId } = req.params;
   const grind = await grindService.getGrindById(grindId);
+  // Remove milestones to keep HTTP requests as light as possible
+  grind.milestones = [];
   res.json(grind).send();
 };
 
@@ -31,21 +33,25 @@ const deleteGrind = (req, res) => {
 
 // MILESTONES
 
-const getMilestone = (req, res) => {
+const getMilestone = async (req, res) => {
   const { grindId, milestoneId } = req.params;
-  res.json(grindService.getMilestone(grindId, milestoneId)).send();
+  const milestone = await grindService.getMilestone(grindId, milestoneId);
+  // Remove milestones to keep HTTP requests as light as possible
+  milestone.activities = [];
+  res.json(milestone).send();
 };
 
-const addMilestone = (req, res) => {
-  const { grindId, name, description, dueDate } = req.body;
-  grindService.addMilestone(grindId, name, description, dueDate);
-  res.status(200).send();
+const addMilestone = async (req, res) => {
+  const { grindId } = req.params;
+  const { name, description, dueDate } = req.body;
+  const insertedMilestone = await grindService.addMilestone(grindId, name, description, dueDate);
+  res.json(insertedMilestone).send();
 };
 
 const editMilestone = (req, res) => {
   const { grindId } = req.params;
   const milestone = req.body;
-  grindService.editMilestone(grindId, milestone.id, milestone);
+  grindService.editMilestone(grindId, milestone._id, milestone);
   res.status(200).send();
 };
 
@@ -57,21 +63,22 @@ const deleteMilestone = (req, res) => {
 
 // ACTIVITIES
 
-const getActivities = (req, res) => {
+const getActivities = async (req, res) => {
   const { grindId, milestoneId, activityId } = req.params;
-  res.json(grindService.getActivities(grindId, milestoneId, activityId)).send();
+  res.json(await grindService.getActivities(grindId, milestoneId, activityId)).send();
 };
 
-const addActivity = (req, res) => {
-  const { grindId, milestoneId, name, dueDate } = req.body;
-  grindService.addActivity(grindId, milestoneId, name, dueDate);
-  res.status(200).send();
+const addActivity = async (req, res) => {
+  const { grindId, milestoneId } = req.params;
+  const { name, dueDate } = req.body;
+  const insertedActivity = await grindService.addActivity(grindId, milestoneId, name, dueDate);
+  res.json(insertedActivity).send();
 };
 
 const editActivity = (req, res) => {
   const { grindId, milestoneId } = req.params;
   const activity = req.body;
-  grindService.editActivity(grindId, milestoneId, activity.id, activity);
+  grindService.editActivity(grindId, milestoneId, activity._id, activity);
   res.status(200).send();
 };
 
--- services/grind.service.js
+++ services/grind.service.js
@@ -1,17 +1,20 @@
 import * as dbService from './db.service.js';
+import { ObjectId } from 'mongodb';
 
 const client = dbService.getConnection();
 const DB_NAME = 'striver';
 const COLLECTION_NAME = 'grinds';
+
 // GRIND
 
 const getGrindById = async (grindId) => {
   await client.connect();
   const db = client.db(DB_NAME);
-  return dbService.getRecord(db, COLLECTION_NAME, grindId);
+  const grind = await dbService.getRecord(db, COLLECTION_NAME, grindId);
+  return grind;
 };
 
-const addGrind = async (striverId, name, description) => {
+const addGrind = async (striverId, name, description, dueDate) => {
   await client.connect();
   const db = client.db(DB_NAME);
   return dbService.insertRecord(db, COLLECTION_NAME, {
@@ -20,14 +23,18 @@ const addGrind = async (striverId, name, description) => {
     milestones: [],
     name,
     description,
-    dueDate: null,
+    dueDate,
   });
 };
 
 const editGrind = async (grindId, grind) => {
   await client.connect();
   const db = client.db(DB_NAME);
-  await dbService.updateRecord(db, COLLECTION_NAME, grindId, grind);
+
+  const oldGrind = await getGrindById(grindId);
+  const newGrind = { ...oldGrind, ...grind };
+
+  await dbService.updateRecord(db, COLLECTION_NAME, grindId, newGrind);
 };
 
 const deleteGrind = async (grindId) => {
@@ -39,39 +46,131 @@ const deleteGrind = async (grindId) => {
 // MILESTONE
 
 const getMilestone = async (grindId, milestoneId) => {
-  // TODO: Implement
-  return {};
+  const grind = await getGrindById(grindId);
+  return grind.milestones.find((milestone) => milestone._id === milestoneId);
 };
 
 const addMilestone = async (grindId, name, description, dueDate) => {
-  // TODO: Implement
+  await client.connect();
+  const db = client.db(DB_NAME);
+
+  const grind = await getGrindById(grindId);
+
+  const milestoneId = ObjectId();
+  grind.milestones.push({
+    _id: milestoneId,
+    activities: [],
+    name,
+    description,
+    dueDate,
+  });
+
+  await dbService.updateRecord(db, COLLECTION_NAME, grindId, grind);
+
+  return milestoneId;
 };
 
 const editMilestone = async (grindId, milestoneId, milestone) => {
-  // TODO: Implement
+  await client.connect();
+  const db = client.db(DB_NAME);
+
+  const grind = await getGrindById(grindId);
+  const oldMilestone = grind.milestones.find(
+    (milestone) => milestone._id.toString() === milestoneId,
+  );
+  const newMilestone = { ...oldMilestone, ...milestone };
+
+  const index = grind.milestones.findIndex(
+    (milestone) => milestone._id.toString() === milestoneId,
+  );
+
+  grind.milestones[index] = newMilestone;
+
+  await dbService.updateRecord(db, COLLECTION_NAME, grindId, grind);
 };
 
 const deleteMilestone = async (grindId, milestoneId) => {
-  // TODO: Implement
+  await client.connect();
+  const db = client.db(DB_NAME);
+
+  const grind = await getGrindById(grindId);
+  grind.milestones = grind.milestones.filter(
+    (milestone) => milestone._id.toString() !== milestoneId,
+  );
+
+  await dbService.updateRecord(db, COLLECTION_NAME, grindId, grind);
 };
 
 // ACTIVITY
 
-const getActivities = async (grindId, milestoneId, activityId) => {
-  // TODO: Implement
-  return [];
+const getActivities = async (grindId, milestoneId) => {
+  const milestone = await getMilestone(grindId, milestoneId);
+  return milestone.activities;
 };
 
 const addActivity = async (grindId, milestoneId, name, dueDate) => {
-  // TODO: Implement
+  await client.connect();
+  const db = client.db(DB_NAME);
+
+  const grind = await getGrindById(grindId);
+
+  const activityId = ObjectId();
+
+  grind.milestones
+    .find((milestone) => milestone._id.toString() === milestoneId)
+    .activities
+    .push({
+      _id: activityId,
+      name,
+      dueDate,
+      completed: false,
+    });
+
+  await dbService.updateRecord(db, COLLECTION_NAME, grindId, grind);
+
+  return activityId;
 };
 
 const editActivity = async (grindId, milestoneId, activityId, activity) => {
-  // TODO: Implement
+  await client.connect();
+  const db = client.db(DB_NAME);
+
+  const grind = await getGrindById(grindId);
+
+  const milestone = grind.milestones.find(
+    (milestone) => milestone._id.toString() === milestoneId,
+  );
+
+  const oldActivity = milestone.activities.find(
+    (activity) => activity._id.toString() === activityId,
+  );
+
+  const newActivity = { ...oldActivity, ...activity };
+
+  const index = milestone.activities.findIndex(
+    (activity) => activity._id.toString() === activityId,
+  );
+
+  milestone.activities[index] = newActivity;
+
+  await dbService.updateRecord(db, COLLECTION_NAME, grindId, grind);
 };
 
 const deleteActivity = async (grindId, milestoneId, activityId) => {
-  // TODO: Implement
+  await client.connect();
+  const db = client.db(DB_NAME);
+
+  const grind = await getGrindById(grindId);
+
+  const milestone = grind.milestones.find(
+    (milestone) => milestone._id.toString() === milestoneId,
+  );
+
+  milestone.activities = milestone.activities.filter(
+    (activity) => activity._id.toString() !== activityId,
+  );
+
+  await dbService.updateRecord(db, COLLECTION_NAME, grindId, grind);
 };
 
 
